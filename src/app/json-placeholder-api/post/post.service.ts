8d89e0533ff9974f40136a966e27dc384990c58b
Ryan de Gruyter <ryan.degruyter@ordina.be>
2017-10-01 21:17:50 +0200

    Fix copy paste

--- src/app/json-placeholder-api/post/post.service.ts
+++ src/app/json-placeholder-api/post/post.service.ts
@@ -8,21 +8,21 @@ import {ApiEnvironment, ApiEnvironmentToken} from '../api-environment';
 export class PostService {
 
   private static readonly PATH: string = 'posts';
-  private usersUrl = '';
+  private postsUrl = '';
 
   constructor(private http: Http,
               @Inject(ApiEnvironmentToken)
               private env: ApiEnvironment) {
-    this.usersUrl = `${env.baseUrl}${PostService.PATH}`;
+    this.postsUrl = `${env.baseUrl}${PostService.PATH}`;
   }
 
   getAll(): Observable<Post[]> {
-    return this.http.get(`${this.usersUrl}`)
+    return this.http.get(`${this.postsUrl}`)
       .map((response: Response) => response.json() || []);
   }
 
   getPost(id: number): Observable<Post> {
-    return this.http.get(`${this.usersUrl}/${id}`)
+    return this.http.get(`${this.postsUrl}/${id}`)
       .map((response: Response) => response.json());
   }
 }
