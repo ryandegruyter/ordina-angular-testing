165abb28258ac06c96bc3f5088b0ab943c1c1294
Dimitri De Kerf <dimidekerf@hotmail.com>
2017-10-19 21:39:45 +0200

    E2E test App component

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,30 @@
-import { OrdinaAngularTestingWorkshopPage } from './app.po';
+import { AppComponent } from './app.po';
+import { browser } from 'protractor';
 
-describe('ordina-angular-testing-workshop App', () => {
-  let page: OrdinaAngularTestingWorkshopPage;
+describe('Home Page', () => {
+    let page: AppComponent;
 
-  beforeEach(() => {
-    page = new OrdinaAngularTestingWorkshopPage();
-  });
+    beforeEach(() => {
+        page = new AppComponent();
+        page.navigateTo();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('should display the correct title', () => {
+        expect(page.getTitleText()).toEqual('NG Square');
+    });
+
+    it('should navigate to the Home page when clicking the brand', () => {
+        page.clickBrand();
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/feed');
+    });
+
+    it('should navigate to the the Feed page when clicking the Feed menu', () => {
+        page.clickFeed();
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/feed');
+    });
+
+    it('should navigate to the Users page when clicking the Users menu', () => {
+        page.clickUsers();
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/users');
+    });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,47 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, $, $$, ElementFinder, ElementArrayFinder } from 'protractor';
+import { Promise } from 'es6-promise';
 
 export class OrdinaAngularTestingWorkshopPage {
-  navigateTo() {
-    return browser.get('/');
-  }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    public navBarItems: ElementArrayFinder = $$('.navbar-item');
+    private navBarBurgerButton: ElementFinder = $('.button.navbar-burger');
+
+    public navigateTo(): Promise<any> {
+        return browser.get('/');
+    }
+
+    public getTitleText(): Promise<string> {
+        return element(by.css('app-root h1')).getText();
+    }
+
+    public clickBrand(): void {
+        const brandNavItem = this.navBarItems.get(0);
+        brandNavItem.click();
+    }
+
+    public clickFeed(): Promise<any> {
+        const feedNavItem = this.navBarItems.get(1);
+        return this.navBarBurgerButton.isDisplayed().then((isDisplayed: boolean) => {
+            if (isDisplayed) {
+                this.navBarBurgerButton.click().then(() => {
+                    feedNavItem.click();
+                });
+            } else {
+                feedNavItem.click();
+            }
+        });
+    }
+
+    public clickUsers(): Promise<any> {
+        const usersNavItem = this.navBarItems.get(2);
+        return this.navBarBurgerButton.isDisplayed().then((isDisplayed: boolean) => {
+            if (isDisplayed) {
+                this.navBarBurgerButton.click().then(() => {
+                    usersNavItem.click();
+                });
+            } else {
+                usersNavItem.click();
+            }
+        });
+    }
 }
